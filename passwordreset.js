ec133e87b030e2528838cfae0cfbbbfdc914fa5e
Baurpower <alexbaur123@gmail.com>
2025-06-07 22:00:35 -0400

    fixed

--- passwordreset.js
+++ passwordreset.js
@@ -2,8 +2,8 @@
 
 // Initialize Supabase client
 const supabase = supabase.createClient(
-  "https://YOUR_PROJECT_ID.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
+  "https://geznczcokbgybsseipjg.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlem5jemNva2JneWJzc2VpcGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMTQyMzQsImV4cCI6MjA2NDg5MDIzNH0.lUx0_kkp9zrqq9rZq6JIRogveyeUSOJ7P87wJUxv9P0"
 );
 
 // Elements
